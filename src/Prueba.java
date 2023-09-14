06693f22d985291def2aa2f750f5afe4dcc25139
ederjgb94 <a2133330155@alumnos.uat.edu.mx>
2023-09-14 10:41:51 -0600

    ok

---
+++ src/Prueba.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Prueba {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Pila p = new Pila();
+        int entrada;
+
+        System.out.println(1);
+
+    }
+}
